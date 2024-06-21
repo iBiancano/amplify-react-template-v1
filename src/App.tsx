51c089b3b1f776285e23ec3a9d9a393f2281429c
ivan.blancas <ivan.blancas@d3clarity.com>
2024-06-21 11:52:01 -0600

    version 1

--- src/App.tsx
+++ src/App.tsx
@@ -42,9 +42,6 @@ function App() {
       </ul>
       <div>
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
       </div>
       <button onClick={signOut}>Sign out</button>
     </main>
